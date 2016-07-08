c65ec202c96dbae1c02d5258956b6889e7efada9
cfvalente <caio99valente@hotmail.com>
2016-07-08 20:54:41 -0300

    Commit 1

---
+++ Source/Cannon/PlayerCannon.cpp
@@ -0,0 +1,97 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Cannon.h"
+#include "PlayerCannon.h"
+
+
+// Sets default values
+APlayerCannon::APlayerCannon()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
+
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("RootComponent"));
+
+	OurCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("OurCamera"));
+	OurVisibleComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("OurVisibleComponent"));
+
+	//OurCamera->SetupAttachment(RootComponent);
+	OurCamera->SetWorldLocation(FVector(-450.0f, 0.0f, 450.0f));
+	OurCamera->SetWorldRotation(FRotator(-15.0f, 0.0f, 0.0f));
+	OurVisibleComponent->SetupAttachment(RootComponent);
+
+
+	CannonBarrel = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("CannonBarrel"));
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> CannonBarrelObject(TEXT("/Game/cano")); // wherein /Game/ is the Content folder.
+	CannonBarrel->SetStaticMesh(CannonBarrelObject.Object);
+
+	CannonBody = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("CannonBody"));
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> CannonBodyObject(TEXT("/Game/canhao")); // wherein /Game/ is the Content folder.
+	CannonBody->SetStaticMesh(CannonBodyObject.Object);
+
+
+}
+
+// Called when the game starts or when spawned
+void APlayerCannon::BeginPlay()
+{
+	CamDir = FVector(0.0f, 0.0f, 0.0f);
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APlayerCannon::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+
+	if (!CamDir.IsZero())
+	{
+		FVector NewLocation = OurCamera->GetComponentLocation() + CamDir * DeltaTime;
+		OurCamera->SetWorldLocation(NewLocation);
+	}
+
+	{
+		CannonBarrel->SetRelativeRotation(CannonBarrel->GetComponentRotation() + FRotator(0.0f, 0.0f, Ang));
+
+	}
+
+
+	/*{
+		FVector NewLocation = CannonBarrel->GetComponentLocation() + FVector(0,0,250) * DeltaTime;
+		CannonBarrel->SetWorldLocation(NewLocation);
+
+	}*/
+
+}
+
+// Called to bind functionality to input
+void APlayerCannon::SetupPlayerInputComponent(class UInputComponent* InputComponent)
+{
+	Super::SetupPlayerInputComponent(InputComponent);
+	InputComponent->BindAxis("MoveZ", this, &APlayerCannon::MoveZ);
+	InputComponent->BindAxis("MoveY", this, &APlayerCannon::MoveY);
+	InputComponent->BindAxis("BarrelRotation", this, &APlayerCannon::MoveTurret);
+
+}
+
+void APlayerCannon::MoveZ(float AxisValue)
+{
+	CamDir.Z = FMath::Clamp(AxisValue, -1.0f, 1.0f) * 1000.0f;
+}
+
+void APlayerCannon::MoveY(float AxisValue)
+{
+	CamDir.Y = FMath::Clamp(AxisValue, -1.0f, 1.0f) * 1000.0f;
+}
+
+void APlayerCannon::MoveTurret(float AxisValue)
+{
+	Ang = FMath::Clamp(AxisValue, -1.0f, 1.0f) * 10.0f;
+	//CamDir.X = FMath::Clamp(AxisValue, -1.0f, 1.0f) * 1000.0f;
+
+
+}
+
---
+++ Source/Cannon/PlayerCannon.h
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Pawn.h"
+#include "PlayerCannon.generated.h"
+
+UCLASS()
+class CANNON_API APlayerCannon : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	APlayerCannon();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void Tick( float DeltaSeconds ) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* InputComponent) override;
+
+
+	FVector CamDir;
+	float Ang;
+
+	void MoveZ(float AxisValue);
+	void MoveY(float AxisValue);
+	void MoveTurret(float AxisValue);
+
+	UCameraComponent* OurCamera;
+	UPROPERTY(EditAnywhere) USceneComponent* OurVisibleComponent;
+
+
+
+
+
+	/** Static Mesh Comp, Set In BP Default Properties */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere) class UStaticMeshComponent* CannonBarrel;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere) class UStaticMeshComponent* CannonBody;
+	
+};
